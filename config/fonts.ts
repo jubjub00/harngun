f44ba3868665250d06d924f19433485ff60e9672
chukeit panpiboon <chukeit.p@lmwn.com>
2024-10-20 18:35:59 +0700

    first commit

---
+++ config/fonts.ts
@@ -0,0 +1,17 @@
+import { Fira_Code as FontMono, Inter as FontSans, IBM_Plex_Sans_Thai_Looped as FontIbm } from "next/font/google";
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const fontMono = FontMono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
+
+export const fontIbm = FontIbm({
+  subsets: ["thai"],
+  variable: "--font-ibm",
+  weight: ['100', '200', '300', '400', '500', '600', '700'],
+});
---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import clsx from "clsx";
+
+import { fontSans } from "@/config/fonts";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai+Looped:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body
+        className={clsx(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
